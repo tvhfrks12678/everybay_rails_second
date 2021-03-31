dbf047d2ee592f37591537078ee89c760f6ab391
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-03-31 11:57:02 +0900

    Finish Chapter-6

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.feature "Projects", type: :feature do
+  # ユーザーは新しいプロジェクトを作成する
+  scenario "user creates a new project" do
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email",	with: user.email
+    fill_in "Password",	with: user.password
+    click_button "Log in"
+
+    expect {
+      click_link "New Project"
+      fill_in "Name",	with: "Test Project" 
+      fill_in "Description",	with: "Trying out Capybara" 
+      click_button "Create Project"
+
+      expect(page).to have_content "Project was successfully created"
+      expect(page).to have_content "Test Project"
+      expect(page).to have_content "Owner: #{user.name}"
+    }.to change(user.projects, :count).by(1)
+  end
+
+  # scenario "guest adds a project" do
+  #   visit projects_path
+  #   save_and_open_page
+  #   click_link "New Project"
+  # end
+
+  # scenario "works with all kinds of HTML elements" do
+  #   visit "/fake/page"
+  #   expect(page).to have_css "h2#subheading"
+  # end
+
+end
+
+
+
